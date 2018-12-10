b7f871cb4aad0f1293754eb5a01dcf9af9f24a29
ElwinBran <elwinbran@gmail.com>
2018-12-10 21:07:27 +0100

    Fixed the api service that is now able to search all recipes.

--- app/src/main/java/com/github/elwinbran/recipeapp/RecipeApiService.java
+++ app/src/main/java/com/github/elwinbran/recipeapp/RecipeApiService.java
@@ -3,8 +3,8 @@ package com.github.elwinbran.recipeapp;
 import retrofit2.Call;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
-import retrofit2.http.Path;
 
 /**
  *
@@ -23,20 +23,8 @@ public interface RecipeApiService
             .build();
 
     /**
-     * The string in the GET annotation is added to the BASE_URL.
-     * <p>
-     * It simply represents the designed layout of the URLs of the numbersapi.com website,
-     * <p>
-     * refer to it in a browser and try. This request will deliver a json stream based on month and
-     * <p>
-     * day of month. It will be put in a DayQuoteTime object by Retrofit.
+     *
      */
-
-    @GET("/{number}/trivia?json")
-    /**
-     * "DayQuoteTime" is the name of the helper class just defined, defining the datamodel, and given as argument.
-     * "getTodaysQuote" is the name of the symbol get method. It can be chosen at wish, as long as it is invoked
-     * with the same name.
-     */
-    Call<Iterable<String>> getQuote(@Path("number") int number);
+    @GET("search?sort=r")
+    Call<Iterable<Recipe>> popularRecipes(@Body String accessToken);
 }
