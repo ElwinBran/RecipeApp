b1b2a62075b74e83a2f44e98dd3561c44af68a25
ElwinBran <elwinbran@gmail.com>
2018-12-10 21:06:59 +0100

    Added an adapter for future use

---
+++ app/src/main/java/com/github/elwinbran/recipeapp/RecipeFragmentPagerAdapter.java
@@ -0,0 +1,27 @@
+package com.github.elwinbran.recipeapp;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class RecipeFragmentPagerAdapter extends FragmentPagerAdapter
+{
+
+    public RecipeFragmentPagerAdapter(FragmentManager manager)
+    {
+        super(manager);
+
+    }
+
+    @Override
+    public Fragment getItem(int i)
+    {
+        return null;
+    }
+
+    @Override
+    public int getCount()
+    {
+        return 0;
+    }
+}
