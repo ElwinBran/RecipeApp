839d25f311d9ec2378e4a2ddde618b43a0be8fa7
ElwinBran <elwinbran@gmail.com>
2018-12-10 21:07:47 +0100

    Added API call that does nothing else yet.

--- app/src/main/java/com/github/elwinbran/recipeapp/MainActivity.java
+++ app/src/main/java/com/github/elwinbran/recipeapp/MainActivity.java
@@ -2,6 +2,11 @@ package com.github.elwinbran.recipeapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -9,7 +14,26 @@ public class MainActivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
+        final String key = "74dea97d3e6df9461980789902998e79";
+        final String accessParameter = "key=" + key;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        RecipeApiService service = RecipeApiService.retrofit.create(RecipeApiService.class);
+        Call<Iterable<Recipe>> call = service.popularRecipes(accessParameter);
+        call.enqueue(new Callback<Iterable<Recipe>>() {
+
+            @Override
+            public void onResponse(Call<Iterable<Recipe>> call, Response<Iterable<Recipe>> response)
+            {
+                Iterable<Recipe> topRecipes = response.body();
+            }
+
+            @Override
+            public void onFailure(Call<Iterable<Recipe>> call, Throwable t)
+            {
+                Log.d("error",t.toString());
+            }
+
+        });
     }
 }
