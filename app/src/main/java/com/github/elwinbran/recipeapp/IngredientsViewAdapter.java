565591d83e9a73d0807360016316f79e9e0c42fc
ElwinBran <elwinbran@gmail.com>
2018-12-12 21:43:08 +0100

    Adapter is almost finished

--- app/src/main/java/com/github/elwinbran/recipeapp/IngredientsViewAdapter.java
+++ app/src/main/java/com/github/elwinbran/recipeapp/IngredientsViewAdapter.java
@@ -4,24 +4,34 @@ import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.ViewGroup;
 
+import java.util.List;
+
 public class IngredientsViewAdapter extends RecyclerView.Adapter<IngredientViewHolder>
 {
+    private final List<String> ingredients;
+
+    public IngredientsViewAdapter(List<String> ingredients)
+    {
+        this.ingredients = ingredients;
+    }
+
     @NonNull
     @Override
     public IngredientViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i)
     {
+        //TODO: inflater code
         return null;
     }
 
     @Override
     public void onBindViewHolder(@NonNull IngredientViewHolder ingredientViewHolder, int i)
     {
-
+        ingredientViewHolder.displayText(this.ingredients.get(i));
     }
 
     @Override
     public int getItemCount()
     {
-        return 0;
+        return this.ingredients.size();
     }
 }
