045497ef0ce45b164fe8c7979115102337f3202e
ElwinBran <elwinbran@gmail.com>
2018-12-12 21:32:03 +0100

    Added generated blank adapter for ingredient list

---
+++ app/src/main/java/com/github/elwinbran/recipeapp/IngredientsViewAdapter.java
@@ -0,0 +1,27 @@
+package com.github.elwinbran.recipeapp;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+public class IngredientsViewAdapter extends RecyclerView.Adapter<IngredientViewHolder>
+{
+    @NonNull
+    @Override
+    public IngredientViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i)
+    {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull IngredientViewHolder ingredientViewHolder, int i)
+    {
+
+    }
+
+    @Override
+    public int getItemCount()
+    {
+        return 0;
+    }
+}
