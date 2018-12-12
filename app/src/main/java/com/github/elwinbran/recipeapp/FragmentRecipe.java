ac156eb9d38c1a9d8d3af91ba1d11a64cd1dc24f
ElwinBran <elwinbran@gmail.com>
2018-12-12 21:12:54 +0100

    Added model code to the fragment

--- app/src/main/java/com/github/elwinbran/recipeapp/FragmentRecipe.java
+++ app/src/main/java/com/github/elwinbran/recipeapp/FragmentRecipe.java
@@ -3,29 +3,46 @@ package com.github.elwinbran.recipeapp;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 
 public class FragmentRecipe extends Fragment
 {
+    final static String MODEL_KEY = "recipe";
+
+    private TextView title;
+
     private ImageView recipePreview;
 
     private RecyclerView ingredientList;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState)
     {
         super.onCreateView(inflater, parent, savedInstanceState);
-        return inflater.inflate(R.layout.fragment_recipe, parent, false);
+        View fragment = inflater.inflate(R.layout.fragment_recipe, parent, false);
+        Bundle args = getArguments();
+        setContent(fragment, args);
+        return fragment;
     }
 
-    @Override
-    public void onViewCreated(View view, Bundle savedInstanceState)
+    private void setContent(View view, Bundle data)
     {
         recipePreview = view.findViewById(R.id.imageView);
         ingredientList = view.findViewById(R.id.recyclerViewIngredients);
+        title = view.findViewById(R.id.textViewRecipeTitle);
+
+        Recipe model = (Recipe) data.getSerializable(MODEL_KEY);
+        title.setText(model.title());
+        Glide.with(this).load(model.imageUrl()).into(recipePreview);
+        //ingredientList.
     }
 
 }
