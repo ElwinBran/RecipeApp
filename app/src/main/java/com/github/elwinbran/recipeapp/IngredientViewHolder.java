046acf3b30868896a16209cf4a8e77a774d90a31
ElwinBran <elwinbran@gmail.com>
2018-12-12 21:39:17 +0100

    Finished ingredient view holder and should be able to behave correctly.

--- app/src/main/java/com/github/elwinbran/recipeapp/IngredientViewHolder.java
+++ app/src/main/java/com/github/elwinbran/recipeapp/IngredientViewHolder.java
@@ -3,12 +3,22 @@ package com.github.elwinbran.recipeapp;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.TextView;
+
 
 public class IngredientViewHolder extends RecyclerView.ViewHolder
 {
 
+    private TextView ingredient;
+
     public IngredientViewHolder(@NonNull View itemView)
     {
         super(itemView);
+        ingredient = itemView.findViewById(R.id.textViewIngredient);
+    }
+
+    public void displayText(String text)
+    {
+        this.ingredient.setText(text);
     }
 }
