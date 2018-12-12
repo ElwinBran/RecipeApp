a252e751b8378debf785f48ef2f986b00ae0f4d6
ElwinBran <elwinbran@gmail.com>
2018-12-12 21:19:13 +0100

    Changed main and now recipes are visbibly loaded and the swiping behaviour is already present

--- app/src/main/java/com/github/elwinbran/recipeapp/MainActivity.java
+++ app/src/main/java/com/github/elwinbran/recipeapp/MainActivity.java
@@ -1,8 +1,13 @@
 package com.github.elwinbran.recipeapp;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -15,11 +20,13 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState)
     {
         final String key = "74dea97d3e6df9461980789902998e79";
+        final Integer recipeCount = 3;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ViewPager viewPager = (ViewPager) findViewById(R.id.viewPager);
-        FragmentPagerAdapter fpAdapter = null;//getSupportFragmentManager();
-        viewPager.setAdapter(fpAdapter);
+        final ViewPager viewPager = (ViewPager) findViewById(R.id.viewPager);
+        final FragmentManager manager = getSupportFragmentManager();
+        //FragmentPagerAdapter fpAdapter = null;//getSupportFragmentManager();
+        //viewPager.setAdapter(fpAdapter);
         RecipeApiService service = RecipeApiService.retrofit.create(RecipeApiService.class);
         Call<SearchResponse> call = service.popularRecipes(key);
         call.enqueue(new Callback<SearchResponse>() {
@@ -29,7 +36,20 @@ public class MainActivity extends AppCompatActivity
             {
                 SearchResponse topRecipes = response.body();
                 Log.d("none", topRecipes.recipes().iterator().next().title());
+                Log.d("none", topRecipes.recipes().iterator().next().imageUrl());
+                List<Recipe> topRecipeList = topRecipes.recipes();
+                int maxRecipeRequirement;
+                if(topRecipeList.size() < recipeCount)
+                {
+                    maxRecipeRequirement = topRecipeList.size();
+                }
+                else
+                {
+                    maxRecipeRequirement = recipeCount;
+                }
+                List<Recipe> requiredRecipes = topRecipes.recipes().subList(0, maxRecipeRequirement);
                 //extract 3 recipes and apply them to the pages
+                viewPager.setAdapter(new RecipeFragmentPagerAdapter(manager, requiredRecipes));
             }
 
             @Override
