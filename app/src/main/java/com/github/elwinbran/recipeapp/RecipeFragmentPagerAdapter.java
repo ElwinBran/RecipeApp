291d4eb25e51c46588e2211a988b127e1243beab
ElwinBran <elwinbran@gmail.com>
2018-12-12 21:18:29 +0100

    Progressed the Adapter so it works but is not finished

--- app/src/main/java/com/github/elwinbran/recipeapp/RecipeFragmentPagerAdapter.java
+++ app/src/main/java/com/github/elwinbran/recipeapp/RecipeFragmentPagerAdapter.java
@@ -1,27 +1,42 @@
 package com.github.elwinbran.recipeapp;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 
+import java.util.List;
+
+/**
+ *
+ *
+ * @author Elwin Slokker
+ */
 public class RecipeFragmentPagerAdapter extends FragmentPagerAdapter
 {
 
-    public RecipeFragmentPagerAdapter(FragmentManager manager)
+    private final List<Recipe> recipes;
+
+    public RecipeFragmentPagerAdapter(FragmentManager manager, List<Recipe> recipes)
     {
         super(manager);
-
+        this.recipes = recipes;
     }
 
     @Override
     public Fragment getItem(int i)
     {
-        return null;
+        Recipe retrieved = recipes.get(i);
+        Fragment fragment = new FragmentRecipe();
+        Bundle args = new Bundle();
+        args.putSerializable(FragmentRecipe.MODEL_KEY, retrieved);
+        fragment.setArguments(args);
+        return fragment;
     }
 
     @Override
     public int getCount()
     {
-        return 0;
+        return this.recipes.size();
     }
 }
