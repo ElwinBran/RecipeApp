103125755a88f24032107aeb6ace3e1f2b0a76ec
ElwinBran <elwinbran@gmail.com>
2018-12-12 21:31:38 +0100

    Added base element for the ingredient list

---
+++ app/src/main/java/com/github/elwinbran/recipeapp/IngredientViewHolder.java
@@ -0,0 +1,14 @@
+package com.github.elwinbran.recipeapp;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+public class IngredientViewHolder extends RecyclerView.ViewHolder
+{
+
+    public IngredientViewHolder(@NonNull View itemView)
+    {
+        super(itemView);
+    }
+}
